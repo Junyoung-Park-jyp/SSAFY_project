fb27e0d91813650f640a63222e46153c20567ab7
박기현 <parkgihyun99@gmail.com>
2024-05-16 13:59:40 +0900

    aa

--- final_pjt_back/final_pjt_back/settings.py
+++ final_pjt_back/final_pjt_back/settings.py
@@ -144,3 +144,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+]
--- final_pjt_back/products/urls.py
+++ final_pjt_back/products/urls.py
@@ -17,9 +17,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('products.urls')),
-    path('accounts/', include('dj_rest_auth.urls')),
-    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
-]
+urlpatterns = []
+
