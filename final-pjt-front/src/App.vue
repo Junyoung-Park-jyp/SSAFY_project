8f0b092f82b74880b4e304319528d8df5e6a86f3
junyoung park <raypark1241@gmail.com>
2024-05-16 11:58:40 +0900

    baseline

---
+++ final-pjt-front/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useArticleStore } from '@/stores/articles'
+import { RouterLink, RouterView } from 'vue-router'
+import { computed, onMounted, ref } from 'vue'
+const store = useArticleStore()
+const isLogin = computed(() => store.isLogin)
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink> | 
+        <RouterLink v-if="!isLogin" :to="{name: 'signup'}">Sign Up | </RouterLink>
+        <RouterLink v-if="!isLogin" :to="{name: 'login'}">Log In | </RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
