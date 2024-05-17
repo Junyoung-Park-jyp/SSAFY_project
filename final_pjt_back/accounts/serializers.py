81feab29d405a1e5d9e29b55f5138f6003970af2
기현 <parkgihyun99@naver.com>
2024-05-17 10:37:15 +0900

    back complete

--- final_pjt_back/accounts/serializers.py
+++ final_pjt_back/accounts/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'saving_products', 'deposit_products', 'bank', 'age']
+        fields = ['id', 'username', 'email', 'saving_products', 'deposit_products', 'bank', 'age', 'current_balance', 'annual_salary']
