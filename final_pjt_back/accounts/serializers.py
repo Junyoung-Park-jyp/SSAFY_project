e42ccce152cb5f2bec26b1ded1c655b26201a47a
raypark1241 <raypark1241@gmail.com>
2024-05-17 13:52:26 +0900

    d

--- final_pjt_back/accounts/serializers.py
+++ final_pjt_back/accounts/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from dj_rest_auth.registration.serializers import RegisterSerializer
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
